b46c7bfa4c21d0c576917d2ab131c772e7193ef1
WilcoApp <hello@wilco.gg>
2024-01-17 16:45:00 +0000

    Initial commit

--- anythink_ack.sh
+++ anythink_ack.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-sleep 10s
+
+# change ports visibility to public to avoid CORS issues
+if [ -n "$CODESPACE_NAME" ]; then
+  .devcontainer/open_port.sh >/dev/null 2>&1
+fi
 
 echo "Welcome to"
 echo "     _                      _     _       _           _     "
@@ -9,4 +13,4 @@ echo "  / ___ \  | | | | | |_| | | |_  | | | | | | | | | | |   <  "
 echo " /_/   \_\ |_| |_|  \__, |  \__| |_| |_| |_| |_| |_| |_|\_\ "
 echo "                    |___/                                   "
 
-echo '\n\e]8;;http://anythink.wilco.gg/chat\e\\Click here\e]8;;\e\\ to go back to Snack'
+echo '\n\e]8;;https://app.wilco.gg/chat\e\\Click here\e]8;;\e\\ to go back to Snack'
